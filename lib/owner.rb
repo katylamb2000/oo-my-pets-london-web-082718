f603782c2d382374d4f25a695b7a8931c30beb71
katy@karstaway.com <katy@karstaway.com>
2018-09-02 14:33:26 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,39 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :fish, :dog, :cat, :name
+  
+  @@all = []
+  
+  def self.reset_all
+ 
+end
+
+def self.all
+  @@all
+end
+
+def self.count
+  @@all.length
+end
+  
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+
+def name 
+  @name = name
+end
+
+def species
+  end
+  
+  def say_species
+  end
+
+def play_with_cats (cat)
+  cat.mood = "happy"
+  end
+
+
 end
